ec143998cf95bbd28fd1e77ab8b235390763f451
Okatev <okatev87@yandex.ru>
2023-06-01 01:12:00 +0300

    1. подготовил тестовые данные (таблицы и наполнение)

---
+++ src/main/resources/data.sql
@@ -0,0 +1,31 @@
+INSERT INTO PUBLIC.USERS ("name", "login", "email", "birthday")
+VALUES ('Andrey', 'AndreyLogin1', 'andrey@mail.ru', '2000-03-13 15:32:54.000');
+
+INSERT INTO PUBLIC.USERS ("name", "login", "email", "birthday")
+VALUES ('Tom', 'Tom123', 'tom@yandex.ru', '1980-03-13 15:32:54.000');
+
+INSERT INTO PUBLIC.USERS ("name", "login", "email", "birthday")
+VALUES ('Tina', 'TinaSmile', 'smile@aport.ru', '2008-03-13 15:32:54.000');
+
+INSERT INTO PUBLIC.USERS ("name", "login", "email", "birthday")
+VALUES ('Kate', 'SunKate', 'sun@yandex.ru', '2008-03-13 15:32:54.000');
+
+INSERT INTO PUBLIC.USERS ("name", "login", "email", "birthday")
+VALUES ('Max', 'Max2000', 'max@rambler.ru', '2000-03-13 15:32:54.000');
+
+
+
+INSERT INTO PUBLIC.FILMS ("name", "description", "releaseDate", "genre", "mpa", "duration")
+VALUES ('Without a face', 'very popular films', '1995-03-13 15:32:54.000', 'Action', 'PG-13', 120);
+
+INSERT INTO PUBLIC.FILMS ("name", "description", "releaseDate", "genre", "mpa", "duration")
+VALUES ('Titanic', 'very popular films', '1998-03-13 15:32:54.000', 'Drama', 'R', 185);
+
+INSERT INTO PUBLIC.FILMS ("name", "description", "releaseDate", "genre", "mpa", "duration")
+VALUES ('Avatar', 'most popular films', '2014-03-13 15:32:54.000', 'Action', 'R', 180);
+
+INSERT INTO PUBLIC.FILMS ("name", "description", "releaseDate", "genre", "mpa", "duration")
+VALUES ('Home alone', 'very funny films', '1993-03-13 15:32:54.000', 'Comedy', 'G', 95);
+
+INSERT INTO PUBLIC.FILMS ("name", "description", "releaseDate", "genre", "mpa", "duration")
+VALUES ('Toys story', 'very popular cartoon', '1996-03-13 15:32:54.000', 'Cartoon', 'G', 100);
